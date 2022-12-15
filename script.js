421dfdccafda73468161f669e05efaef94f85af8
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-12-15 13:28:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-// setInterval(moveBackground, 500);
-// function moveBackground() {
-
-// }
-
-let canvas = document.getElementById('canvas');
+// button element, canvas, and images
+let btn = document.getElementById('start-btn');
+let cnv = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 let img = document.getElementById('background');
-ctx.drawImage(img, 10, 10);
+let img2 = document.getElementById('background-extension');
+
+cnv.width = 1230;
+cnv.height = 800;
+
+let background1X = 0;
+let background2X = 1230;
+
+//event listener
+btn.addEventListener('click', submitBtnClicked);
+
+// background photo
+requestAnimationFrame(backgroundMvmt);
+function backgroundMvmt() {
+    // LOGIC
+    background1X -= 5;
+    if (background1X < -1000) {
+        background1X = 1000;
+    }
+    background2X -= 5;
+    if (background2X < -1000) {
+        background2X = 1000;
+    }
+
+    // DRAWING
+    ctx.drawImage(img, background1X, 0, cnv.width, cnv.height);
+    ctx.drawImage(img2, background2X, 0, cnv.width, cnv.height);
+    requestAnimationFrame(backgroundMvmt);
+}
+
+function submitBtnClicked() {
+
+}
