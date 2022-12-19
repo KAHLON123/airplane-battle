bf5b7d3308b909aa3196c38b0c348d24d90b4389
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-12-19 13:28:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let ctx = canvas.getContext('2d');
 let img = document.getElementById('background');
 let img2 = document.getElementById('background-extension');
 let plane = document.getElementById('playable-character');
+
 //obstacle images
 let articuno = document.getElementById('art-obstacle');
 let moltres = document.getElementById('mol-obstacle');
 let zapdos = document.getElementById('zap-obstacle');
+let houseOne = document.getElementById('house-obstacle');
 
 cnv.width = 1230;
 cnv.height = 800;
@@ -20,7 +22,7 @@ let planeX = 90;
 let planeY = 300;
 
 // ARRAY OF OBSTACLES
-let obstacle = [{ objectID: 'articuno', xStart: 700, yStart: 300, yEnd: 320 }, { objectID: 'moltres', xStart: 700, yStart: 550, yEnd: 570 }, { objectID: 'zapdos', xStart: 700, yStart: 400, yEnd: 420 }];
+let obstacle = [{ objectID: 'articuno', xStart: 1700, yStart: 300, yEnd: 380 }, { objectID: 'moltres', xStart: 3500, yStart: 550, yEnd: 630 }, { objectID: 'zapdos', xStart: 3000, yStart: 400, yEnd: 420 }, { objectID: 'house1', xStart: 700, yStart: 430, yEnd: 650 }];
 
 //event listener
 btn.addEventListener('click', start);
@@ -29,42 +31,79 @@ document.addEventListener('keydown', planeMvmt);
 function planeMvmt(e) {
     //W
     if (e.keyCode === 87) {
-        planeY += 5;
+        planeY -= 5;
         //W
     } else if (e.keyCode === 83) {
-        planeY -= 5;
+        planeY += 5;
         //S
     } else if (e.keyCode === 65) {
         planeX -= 5;
         //A
-    } else if (e.keycode === 68) {
-        planeX += 5;
+    } else if (e.keyCode === 68) {
+        planeX += 10;
         //D
     }
 }
 
 // background photo
 function start() {
-    // LOGIC
-    background1X -= 2;
-    if (background1X < -1230) {
-        background1X = 1230;
+    //if nothing has been hit, then run normal game code
+    if (detectCollision() === false) {
+        // LOGIC
+        background1X -= 2;
+        if (background1X < -1230) {
+            background1X = 1230;
+        }
+        background2X -= 2;
+        if (background2X < -1230) {
+            background2X = 1230;
+        }
+
+        //Moving Obstacles
+        // pokemon/birds
+        for (let i = 0; i < 3; i++) {
+            if (obstacle[i].xStart > -80) {
+                obstacle[i].xStart -= 5;
+            } else {
+                obstacle[i].xStart += 9000;
+            }
+        }
+
+        // buildings
+        // for (let i = 3; i < 3; i++) {
+        if (obstacle[3].xStart > -250) {
+            obstacle[3].xStart -= 2
+        } else {
+            obstacle[3].xStart += 15000;
+        }
+        // }
+
+        // DRAWING
+        //background
+        ctx.drawImage(img, background1X, 0, cnv.width, cnv.height);
+        ctx.drawImage(img2, background2X, 0, cnv.width, cnv.height);
+
+        //playable character
+        ctx.drawImage(plane, planeX, planeY, 130, 80);
+
+        //obstacles
+        ctx.drawImage(articuno, obstacle[0].xStart, obstacle[0].yStart, 80, 80);
+        ctx.drawImage(moltres, obstacle[1].xStart, obstacle[1].yStart, 80, 80);
+        ctx.drawImage(zapdos, obstacle[2].xStart, obstacle[2].yStart, 80, 80);
+        ctx.drawImage(houseOne, obstacle[3].xStart, obstacle[3].yStart, 250, 250);
+        requestAnimationFrame(start);
+    } else {
+        console.log('hit');
     }
-    background2X -= 2;
-    if (background2X < -1230) {
-        background2X = 1230;
-    }
-
-    //moving obstacles
-
-
-    // DRAWING
-    ctx.drawImage(img, background1X, 0, cnv.width, cnv.height);
-    ctx.drawImage(img2, background2X, 0, cnv.width, cnv.height);
-    ctx.drawImage(plane, planeX, planeY, 130, 80);
-    ctx.drawImage(articuno, 700, obstacle[0].yStart, 80, 80);
-    ctx.drawImage(moltres, 700, obstacle[1].yStart, 80, 80);
-    ctx.drawImage(zapdos, 700, obstacle[2].yStart, 80, 80);
-    requestAnimationFrame(start);
 }
 
+function detectCollision() {
+    const planeYEnd = planeY + 80;
+    for (let i = 0; i < obstacle.length; i++) {
+        if (obstacle[i].yStart > planeY && obstacle[i].yEnd < planeYEnd) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
