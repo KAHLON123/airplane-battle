53e067da86a75546bf7c1682f45664925ccfffb4
KAHLON123 <h.kahlon5@share.epsb.ca>
2022-12-21 13:12:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let articuno = document.getElementById('art-obstacle');
 let moltres = document.getElementById('mol-obstacle');
 let zapdos = document.getElementById('zap-obstacle');
 let houseOne = document.getElementById('house-obstacle');
+let skyscraper = document.getElementById('skyscraper-obstacle');
 
 cnv.width = 1230;
 cnv.height = 800;
@@ -20,9 +21,10 @@ let background1X = 0;
 let background2X = 1230;
 let planeX = 90;
 let planeY = 300;
+let b = '';
 
 // ARRAY OF OBSTACLES
-let obstacle = [{ objectID: 'articuno', xStart: 1700, xEnd: 1780, yStart: 300, yEnd: 380 }, { objectID: 'moltres', xStart: 3500, xEnd: 3580, yStart: 550, yEnd: 630 }, { objectID: 'zapdos', xStart: 3000, xEnd: 3080, yStart: 400, yEnd: 420 }, { objectID: 'house1', xStart: 700, xEnd: 950, yStart: 430, yEnd: 650 }];
+let obstacle = [{ objectID: 'articuno', xStart: 1700, xEnd: 1780, yStart: 300, yEnd: 380 }, { objectID: 'moltres', xStart: 3500, xEnd: 3580, yStart: 550, yEnd: 630 }, { objectID: 'zapdos', xStart: 3000, xEnd: 3080, yStart: 400, yEnd: 420 }, { objectID: 'house1', xStart: 700, xEnd: 950, yStart: 430, yEnd: 680 }, { objectID: 'skyscraper', xStart: 1000, xEnd: 1250, yStart: 400, yEnd: 580 }];
 
 //event listener
 btn.addEventListener('click', start);
@@ -37,7 +39,7 @@ function planeMvmt(e) {
         planeY += 5;
         //S
     } else if (e.keyCode === 65) {
-        planeX -= 5;
+        planeX -= 20;
         //A
     } else if (e.keyCode === 68) {
         planeX += 10;
@@ -58,9 +60,8 @@ function start() {
         if (background2X < -1230) {
             background2X = 1230;
         }
-
         //Moving Obstacles
-        // pokemon/birds
+        // pokemon
         for (let i = 0; i < 3; i++) {
             if (obstacle[i].xStart > -80) {
                 obstacle[i].xStart -= 5;
@@ -70,47 +71,46 @@ function start() {
                 obstacle[i].xEnd += 9000;
             }
         }
-
         // buildings
-        // for (let i = 3; i < 3; i++) {
-        if (obstacle[3].xStart > -250) {
-            obstacle[3].xStart -= 2;
-            obstacle[3].xEnd -= 2;
-        } else {
-            obstacle[3].xStart += 15000;
-            obstacle[3].xEnd += 15000;
+        for (let i = 3; i < obstacle.length; i++) {
+            if (obstacle[i].xStart > -250) {
+                obstacle[i].xStart -= 2;
+                obstacle[i].xEnd -= 2;
+            } else {
+                obstacle[i].xStart += 15000;
+                obstacle[i].xEnd += 15000;
+            }
         }
 
         // DRAWING
         //background
         ctx.drawImage(img, background1X, 0, cnv.width, cnv.height);
         ctx.drawImage(img2, background2X, 0, cnv.width, cnv.height);
-
         //playable character
         ctx.drawImage(plane, planeX, planeY, 130, 80);
-
         //obstacles
         ctx.drawImage(articuno, obstacle[0].xStart, obstacle[0].yStart, 80, 80);
         ctx.drawImage(moltres, obstacle[1].xStart, obstacle[1].yStart, 80, 80);
         ctx.drawImage(zapdos, obstacle[2].xStart, obstacle[2].yStart, 80, 80);
         ctx.drawImage(houseOne, obstacle[3].xStart, obstacle[3].yStart, 250, 250);
+        ctx.drawImage(skyscraper, obstacle[4].xStart, obstacle[4].yStart, 150, 250);
+
         requestAnimationFrame(start);
     } else {
         console.log('hit');
     }
 }
 
-
 function detectCollision() {
     let planeYEnd = +planeY + 80;
     let planeXEnd = +planeX + 130;
     for (let i = 0; i < obstacle.length; i++) {
-        // contact at right, contact at top, contact at left, contact at bottom
-        //if left side of object meets right side of character OR (||)
-        if (planeXEnd > obstacle[i].xStart) {
-            return true;
+        // OR (||)
+        if (planeX < obstacle[i].xEnd && planeXEnd > obstacle[i].xStart && planeY < obstacle[i].yEnd && planeYEnd > obstacle[i].yStart) {
+            b = true;
         } else {
-            return false;
+            b = false;
         }
     }
+    return b;
 }
